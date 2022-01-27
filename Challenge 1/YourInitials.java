1792eb358a8c39597fe69d11fca81ce1b92edf83
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2022-01-27 16:19:41 -0600

    Challenge 1, print my initials in different lines

---
+++ Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+  public static void main(String[] args) {
+    // print initials here.
+    System.out.println("  A     SSS ");
+    System.out.println(" A A   S   S");
+    System.out.println("A   A  S    ");
+    System.out.println("AAAAA   SSS ");
+    System.out.println("A   A      S");
+    System.out.println("A   A  S   S");
+    System.out.println("A   A   SSS ");
+  }
+}
