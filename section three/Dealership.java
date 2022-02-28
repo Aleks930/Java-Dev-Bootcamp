58c926225d4699e7a628598c9d982fcc64e776ad
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2022-02-27 18:34:10 -0600

    Dealership project (part 2 & 3) completed.

--- section three/Dealership.java
+++ section three/Dealership.java
@@ -8,8 +8,39 @@ public class Dealership {
     System.out.println(" - Select option 'b' to sell a car");
     String option = scan.nextLine();
     switch (option) {
-      case "a": System.out.println("You chose option " + option); break;
-      case "b": System.out.println("You chose option " + option); break;
+      case "a":
+        System.out.println("What is your budget?");
+        int budget = scan.nextInt();
+        if(budget >= 10000) {
+          System.out.println("Great! A Nissan Altima is available");
+          System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+          scan.nextLine();
+          String insurance = scan.nextLine();
+          System.out.println("\nDo you have a license? Write 'yes' or 'no'");
+          String license = scan.nextLine();
+          System.out.println("\nWhat is your credit score?");
+          int creditScore = scan.nextInt();
+          if(insurance.equals("yes") && license.equals("yes") && creditScore > 660) {
+            System.out.println("\nSold! Pleasure doing business with you.");
+          } else {
+            System.out.println("\nWe are sorry. You are not eligible.");
+          }
+        } else {
+          System.out.println("\nWe don't sell cars under $10,000. Sorry!");
+        }
+      break;
+      case "b":
+        System.out.println("\nWhat is your car valued at?");
+        int value = scan.nextInt();
+        System.out.println("\nWhat is your selling price?");
+        int price = scan.nextInt();
+
+        if(value > price && price < 30000) {
+          System.out.println("\nWe will buy your car. A pleasure doing business with you!");
+        } else {
+          System.out.println("\nSorry, we are not interested!");
+        }
+      break;
       default: System.out.println("Invalid option");
     }
     scan.close();
