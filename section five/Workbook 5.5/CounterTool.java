8d9cba32cc382f403615007d8ed633d4bb287bbd
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2022-03-07 17:24:18 -0600

    Workbook 5.5  done, Counter tool by X steps to an X number. For loop

---
+++ section five/Workbook 5.5/CounterTool.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CounterTool {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("I hear you like to count by threes\n");
+        System.out.println("User: It depends.\n");
+        System.out.println("Oh, ok...");
+
+      /* Task 1 – Get integers from the user.
+
+          1.   print : Pick a number to count by:
+               pick up the user's answer
+
+          2.   print : Pick a number to start counting from:
+               pick up the user's answer
+
+          3.    print : Pick a number to count to:
+                pick up the user's answer
+
+        */
+        System.out.print(" 1. Pick a number to count by: ");
+        int count = scan.nextInt();
+        System.out.print(" 2. Pick a number to start counting from: ");
+        int countingFrom = scan.nextInt();
+        System.out.print("  3. Pick a number to count to: ");
+        int countTo  = scan.nextInt();
+
+      /* Task 2 – Create a for loop that:
+            1. starts from the second number.
+            2. stops at the third number.
+            3. counts in steps of the first.
+      */
+        for (int i = countingFrom; i <= countTo; i += count) {
+          System.out.print(i + " ");
+        }
+        scan.close();
+    }
+}
