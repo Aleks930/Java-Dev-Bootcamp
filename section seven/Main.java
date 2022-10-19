1c2f92dbf7fa26231dfb0110fa6eed4684131766
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2022-10-19 18:53:03 -0500

    Sec7 progress

---
+++ section seven/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car nissan = new Car();
+
+        System.out.println(nissan.make);
+        System.out.println(nissan.price);
+        System.out.println(nissan.year);
+        System.out.println(nissan.color);
+    }
+}
