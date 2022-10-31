96f4b2917a04684096056319b65d27e2a09c9835
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2022-10-31 14:35:38 -0600

    section 7, setters

--- section seven/Main.java
+++ section seven/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
         Car dodge = new Car("Dodge", 8500, 2019, "blue");
 
+        nissan.setColor("Jet black");
+        dodge.setColor("Jet black");
+
+        nissan.setPrice(nissan.getPrice() / 2);
+        dodge.setPrice(dodge.getPrice() / 2);
+
         System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
 
         System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
