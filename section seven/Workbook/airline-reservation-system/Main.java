1f57644dc990736823284d6055b107a2f5304e69
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2023-02-07 18:56:44 -0600

    Workbook 7.4

--- section seven/Workbook/airline-reservation-system/Main.java
+++ section seven/Workbook/airline-reservation-system/Main.java
@@ -1,13 +1,23 @@
 public class Main {
   
     public static void main(String[] args) {
-        Person rayan = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
-        rayan.setSeatNumber(10);
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+        person.setSeatNumber(10);
 
-        System.out.println("Name: " + rayan.getName() + "\n" 
-        +                  "Nationality: " + rayan.getNationality() + "\n" 
-        +                  "Date of birth: " + rayan.getDateOfBirth() + "\n" 
-        +                  "Seat number: " + rayan.getSeatNumber() + "\n");
+        Person twin = new Person(person); // creates a copy of the first object.
+        twin.setName("Jad Slim"); // updates name field of the second object.
+        twin.setSeatNumber(3); // updates the seat number of the second object.
+
+        System.out.println("Name: " + person.getName() + "\n" 
+        +                  "Nationality: " + person.getNationality() + "\n" 
+        +                  "Date of birth: " + person.getDateOfBirth() + "\n" 
+        +                  "Seat number: " + person.getSeatNumber() + "\n");
+
+        System.out.println("Name: " + twin.getName() + "\n" 
+                        + "Nationality: " + twin.getNationality() + "\n" 
+                        + "Date of Birth: " + twin.getDateOfBirth() + "\n" 
+                        + "Seat Number: " + twin.getSeatNumber() + "\n");
+
 
     }
   
