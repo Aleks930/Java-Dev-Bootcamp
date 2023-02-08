fbf72f27cb5f517b812bf2f0bf131348fd61b20a
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2023-02-07 18:30:09 -0600

    Workbook 7.3

--- section seven/Workbook/airline-reservation-system/Main.java
+++ section seven/Workbook/airline-reservation-system/Main.java
@@ -1,14 +1,13 @@
-import java.util.Arrays;
-
 public class Main {
   
     public static void main(String[] args) {
         Person rayan = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
+        rayan.setSeatNumber(10);
 
-        System.out.println("Name: " + rayan.name + "\n" 
-        +                  "Nationality: " + rayan.nationality + "\n" 
-        +                  "Date of birth: " + rayan.dateOfBirth + "\n" 
-        +                  "Seat number: " + rayan.seatNumber + "\n");
+        System.out.println("Name: " + rayan.getName() + "\n" 
+        +                  "Nationality: " + rayan.getNationality() + "\n" 
+        +                  "Date of birth: " + rayan.getDateOfBirth() + "\n" 
+        +                  "Seat number: " + rayan.getSeatNumber() + "\n");
 
     }
   
