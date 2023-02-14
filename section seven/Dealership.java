040417b24a7c8c1a444074e6be052517d7345416
Alex Soto <55665154+Aleks930@users.noreply.github.com>
2023-02-14 13:57:21 -0600

    Finalizing Dealership application

--- section seven/Dealership.java
+++ section seven/Dealership.java
@@ -9,4 +9,18 @@ public class Dealership {
         }
     }
 
+    public void sell(int index) {
+        this.cars[index].drive();
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.cars.length; i++) {
+            temp += "Parking Spot: " + i + "\n";
+            String carDescription = this.cars[i].toString();
+            temp += carDescription + "\n";
+        }
+        return temp;
+    }
+
 }
